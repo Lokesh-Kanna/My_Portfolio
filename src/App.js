032540e181f3c18b362336a2b39f2289ab151634
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-20 14:20:19 +0530

    Added Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+      </Routes>
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Lokesh Kanna
         </a>
-      </header>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <Link class="nav-link active" to="">
+              Home <span class="sr-only">(current)</span>
+            </Link>
+            <Link class="nav-link" to="">
+              About
+            </Link>
+            <Link class="nav-link" to="">
+              Skills
+            </Link>
+            <Link class="nav-link" to="">
+              Projects
+            </Link>
+            <Link class="nav-link" to="">
+              Experience
+            </Link>
+            <Link class="nav-link" to="">
+              Achievements
+            </Link>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
